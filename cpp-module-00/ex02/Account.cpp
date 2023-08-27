fae7f955c2b9f048f269e4ff21d1a538ab01c7c1
Jinho Choi <dandyday31@gmail.com>
2023-08-27 16:22:19 +0900

    Add: module 00 ex02

--- cpp-module-00/ex02/Account.cpp
+++ cpp-module-00/ex02/Account.cpp
@@ -6,7 +6,67 @@
 /*   By: jinhchoi <jinhchoi@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/27 10:50:31 by jinhchoi          #+#    #+#             */
-/*   Updated: 2023/08/27 10:50:32 by jinhchoi         ###   ########.fr       */
+/*   Updated: 2023/08/27 16:22:01 by jinhchoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Account.hpp"
+
+Account::_nbAccounts = 0;
+Account::_totalAmount = 0;
+Account::_totalNbDeposits = 0;
+Account::_totalNbWithdrawals = 0;
+
+int Account::getNbAccounts(void)
+{
+	return 0;
+}
+
+int Account::getTotalAmount(void)
+{
+	return 0;
+}
+
+int Account::getNbDeposits(void)
+{
+	return 0;
+}
+
+int Account::getNbWithdrawals(void)
+{
+	return 0;
+}
+
+void Account::displayAccountsInfos(void)
+{
+}
+
+Account::Account(int initial_deposit)
+{
+}
+
+Account::~Account(void)
+{
+}
+
+void Account::makeDeposit(int deposit)
+{
+}
+
+bool Account::makeWithdrawal(int withdrawal)
+{
+	return false;
+}
+
+int Account::checkAmount(void) const
+{
+	return 0;
+}
+
+void Account::displayStatus(void) const
+{
+}
+
+void Account::_displayTimestamp(void)
+{
+}
