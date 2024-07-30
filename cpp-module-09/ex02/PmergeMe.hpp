a5357363c10cff2189c553e2942edc2d6ad30870
Jinho Choi <dandyday31@gmail.com>
2024-07-30 20:01:45 +0900

    add: module 09 ex02
    added base files

---
+++ cpp-module-09/ex02/PmergeMe.hpp
@@ -0,0 +1,15 @@
+#ifndef PMERGEME_HPP
+#define PMERGEME_HPP
+
+class PmergeMe
+{
+public:
+	PmergeMe();
+	PmergeMe(const PmergeMe &other);
+	PmergeMe &operator=(const PmergeMe &other);
+	~PmergeMe();
+
+private:
+};
+
+#endif
---
+++ cpp-module-09/ex02/main.cpp
@@ -0,0 +1,8 @@
+#include "PmergeMe.hpp"
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+
+	return 0;
+}
